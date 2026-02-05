46633b58978a5957e6d6117627738f6f02a0081a
kanyapaspanganin13-afk <kanyapas.panganin13@gmail.com>
2026-02-05 15:43:02 +0700

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,1 +1,41 @@
+const CACHE_NAME = 'barber-v3'; // 🔥 เปลี่ยนทุกครั้งที่แก้โค้ด
 
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600&display=swap'
+];
+
+// install
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+// activate (ลบ cache เก่า)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
