09a5225dc55eacdd8f432a087b0565e0d1fceca3
kanyapaspanganin13-afk <kanyapas.panganin13@gmail.com>
2026-09-18 10:56:57 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2125,7 +2125,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function loadHistMonth() {
     const $ = (id) => document.getElementById(id);
 
-    const picker = $("histMonth") \vert{}\vert{} $("monthlyReportPicker");
+    const picker = $("histMonth") || $("monthlyReportPicker");
     let m = picker ? picker.value : '';
     if (!m) {
         const now = new Date();
